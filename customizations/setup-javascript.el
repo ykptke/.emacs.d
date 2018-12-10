ea8a23a37e2ede7efafbe6df772ec523e8729c42
ykptke <ykpteke@gmail.com>
2018-12-10 09:58:52 +0300

    rjsx-mode set default for js

--- customizations/setup-javascript.el
+++ customizations/setup-javascript.el
@@ -1,6 +1,6 @@
 ;; install it major mode for JavaScript editing
 
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . rjsx-mode))
 
 ;; indent
