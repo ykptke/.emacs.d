7ca8c98b58d3e22fc2620a2519785d622d33a5ca
yakupteke <yakup@jotform.com>
2019-08-02 16:08:25 +0300

    css indent

--- customizations/misc.el
+++ customizations/misc.el
@@ -12,6 +12,7 @@
 (setq web-mode-markup-indent-offset 2)
 ;; CSS offset indentation
 (setq web-mode-css-indent-offset 2)
+(setq css-indent-offset 2)
 ;; Script/code offset indentation (for JavaScript, Java, PHP, Ruby, Go, VBScript, Python, etc.)
 (setq web-mode-code-indent-offset 2)
 
