316e00a6a62c417424f6b50dd598e1895cf34c31
yakupteke <yakup@jotform.com>
2019-09-25 14:27:31 +0300

    eslint from local node_modules

--- customizations/setup-flycheck.el
+++ customizations/setup-flycheck.el
@@ -17,3 +17,16 @@
 
 ;; enable typescript-tslint checker
 (flycheck-add-mode 'typescript-tslint 'web-mode)
+
+;; Change programmatically flycheck-javascript-eslint-executable for local node_modules installation
+(defun my/use-eslint-from-node-modules ()
+ (let* ((root (locate-dominating-file
+               (or (buffer-file-name) default-directory)
+               "node_modules"))
+        (eslint
+         (and root
+              (expand-file-name "node_modules/.bin/eslint"
+                                root))))
+   (when (and eslint (file-executable-p eslint))
+     (setq-local flycheck-javascript-eslint-executable eslint))))
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
