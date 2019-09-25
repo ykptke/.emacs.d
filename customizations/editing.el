703fd7e6311a14e86c2dc7bb645cb26cbeab6a2e
yakupteke <yakup@jotform.com>
2019-09-25 16:07:13 +0300

    fixed dumb-jump-back shortcut command

--- customizations/editing.el
+++ customizations/editing.el
@@ -82,6 +82,6 @@
 ;; jump to definition `https://github.com/jacktasia/dumb-jump`
 (dumb-jump-mode)
 (global-set-key (kbd "M-g j") 'dumb-jump-go)
-(global-set-key (kbd "M-g b") 'dumb-jump-go-back)
+(global-set-key (kbd "M-g b") 'dumb-jump-back)
 (global-set-key (kbd "M-g o") 'dumb-jump-go-other-window)
 (global-set-key (kbd "M-g l") 'dumb-jump-quick-look)
