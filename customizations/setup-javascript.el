2c25db77641940f220eaa0f5badf5684fd9f6a9e
yakupteke <yakup@jotform.com>
2020-02-17 17:26:23 +0300

    set js indent level

--- customizations/setup-javascript.el
+++ customizations/setup-javascript.el
@@ -3,6 +3,9 @@
 (require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
+;; indent
+(setq js-indent-level 2)
+
 ;; Better imenu
 (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
 
