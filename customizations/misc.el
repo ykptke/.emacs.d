4815d995b59921d508c62dec38b28fa2b2c56e4d
yakupteke <yakup@jotform.com>
2020-02-19 16:12:20 +0300

    close all buffers

--- customizations/misc.el
+++ customizations/misc.el
@@ -18,3 +18,17 @@
 
 (when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize))
+
+;; close all buffers
+(defun kill-other-buffers ()
+  "Kill all buffers except current one and toolkit (*Messages*, *scratch*). Close other windows."
+  (interactive)
+  (mapc 'kill-buffer (cl-remove-if
+                      (lambda (x)
+                        (or
+                         (eq x (current-buffer))
+                         (member (buffer-name x) '("*Messages*" "*scratch*"))))
+                      (buffer-list)))
+  (delete-other-windows))
+
+(global-set-key (kbd "C-x a k") 'kill-other-buffers)
