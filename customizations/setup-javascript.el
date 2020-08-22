8a69711c6e28737b2bbb35ef3ac0c6607b7fac49
yakupteke <yakup@jotform.com>
2020-08-22 13:53:27 +0300

    use rjsx for all

--- customizations/setup-javascript.el
+++ customizations/setup-javascript.el
@@ -1,7 +1,6 @@
 (require 'js2-mode)
 
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
+(add-to-list 'auto-mode-alist '("\\.js*\\'" . rjsx-mode))
 
 ;; indent
 (setq js-indent-level 2)
